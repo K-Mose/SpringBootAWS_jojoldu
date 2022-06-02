ae4c5ef91bc6a357617f0a52985cd82dfdecc81e
Mose <km_413@naver.com>
2022-06-02 17:09:25 +0900

    Lombok DTO Test

--- src/test/java/springboot/web/HelloControllerTest.java
+++ src/test/java/springboot/web/HelloControllerTest.java
@@ -4,15 +4,17 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.hamcrest.Matchers.is;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import springboot.web.dto.HelloResponseDto;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 import static org.junit.Assert.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = HelloController.class)
@@ -30,5 +32,19 @@ public class HelloControllerTest {
                     .andExpect(status().isOk())
                     .andExpect(content().string(hello_upper));
 
+    }
+
+    @Test
+    public void helloDto_return_test() throws Exception {
+        String name = "Mose";
+        int amount = 999;
+
+        mvc.perform(
+                get("/hello/dto")
+                        .param("name", name)
+                        .param("amount", String.valueOf(amount))
+        ).andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 }
