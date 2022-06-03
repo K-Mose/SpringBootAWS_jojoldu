0592004eed3c25e4e38d6c62dea7fd5174ce5067
Mose <km_413@naver.com>
2022-06-03 14:17:59 +0900

    Mustache page Test

---
+++ src/test/java/springboot/web/IndexControllerTest.java
@@ -0,0 +1,26 @@
+package springboot.web;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class IndexControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void mustache_index_test() {
+        String body = this.restTemplate.getForObject("/", String.class);
+
+        assertThat(body).contains("Spring Boot");
+    }
+}
